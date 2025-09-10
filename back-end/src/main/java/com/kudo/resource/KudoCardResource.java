7b2182989596e8573d38375ca39899b4285c0c7e
Luke Murphy <lmurphy6@oswego.edu>
2025-09-10 12:52:55 -0400

    - Added resources folder - Added resource for Kudo card endpoints

---
+++ back-end/src/main/java/com/kudo/resource/KudoCardResource.java
@@ -0,0 +1,19 @@
+package com.kudo.resource;
+
+import java.util.Properties;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@ApplicationScoped
+@Path("/kudocard")
+public class KudoCardResource {
+
+    //Endpoints go here...
+
+}
