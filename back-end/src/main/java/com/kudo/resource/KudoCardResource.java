eeb7c1cbb665442764d430a164d928b8cb224dec
Luke Murphy <lmurphy6@oswego.edu>
2025-09-12 14:20:36 -0400

    - Renamed endpoint /kudo-list to /list for simplicity - Replaced /list PathParam "username" with QueryParam "user_id" - Created wrapper CardIdList for list of cardIds returned by the /list endpoint to allow for automatic JSON conversion - TODO: Implement POST for kudos card

--- back-end/src/main/java/com/kudo/resource/KudoCardResource.java
+++ back-end/src/main/java/com/kudo/resource/KudoCardResource.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import com.kudo.model.CardIdList;
 import jakarta.annotation.Resource;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.json.Json;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.UUID;
 
 @ApplicationScoped
 @Path("kudo-card")
@@ -34,32 +34,37 @@ public class KudoCardResource {
         return "Hello World!";
     }
 
+    //Takes a JSON which represents a Kudocard object and Creates a new entry in the KUDOS_CARD table of the database
+    //Using the supplied information
+    //TODO: Implement POST for creating Kudos card
+
     //Returns list of all IDs pertaining to Kudos which are owned by this user
+    //UUID should be in the format X-X-X-X-X where each X is a string of alphanumerics
     @GET
-    @Path("{username}/kudo-list")
+    @Path("list")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Integer> getKudoList(@PathParam("username") String username) {
+    public CardIdList getKudoList(@QueryParam("user_id") UUID user_id) {
         try (Connection conn = dataSource.getConnection(); //establish database connection
              PreparedStatement stmt = conn.prepareStatement("SELECT card_id FROM KUDOS_CARDS WHERE recipient_id = ?;");){//Static elements of query
-            stmt.setString(1,username); //form the query
+            stmt.setObject(1,user_id); //form the query
 
             ResultSet rs = stmt.executeQuery(); //execute query to obtain list of IDs
 
-            List<Integer> card_ids = new ArrayList<>(); //List which will be filled with card_ids from the result set
+            List<Integer> cardIds = new ArrayList<>(); //List which will be filled with card_ids from the result set
             while (rs.next()) {
-                card_ids.add(rs.getInt("card_id")); //add ids to list
+                cardIds.add(rs.getInt("card_id")); //add ids to list
             }
 
-            //List should automatically be converted to JSON?
-            return card_ids;
+            //Wrap list as CardIdList
+            //CardIdList should automatically be converted to JSON due to the MIME type?
+            return new CardIdList(cardIds);
 
         } catch (SQLException e) {
             //TODO: Handle SQLException (probably return 404)
             throw new RuntimeException(e);
         }
-
-
     }
+
 
 
 }
